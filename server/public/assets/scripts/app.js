20bf0230a1623d30e6c3fc70befe3dd5d2b4b191
lahusiak <lahusiak@gmail.com>
2015-11-02 19:17:35 -0600

    Hard Mode Complete

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,10 +1,10 @@
 console.log("I'm an animal!");
+var values = {};
 
 $(document).ready(function(){
+
     $('#search').submit(function(event){
         event.preventDefault();
-        var values = {};
-
         $.each($(this).serializeArray(), function(i, field){
             values[field.name] = field.value;
         });
@@ -16,17 +16,24 @@ $(document).ready(function(){
             success: function(data){
                 console.log("Meow", data);
             appendDom(data);
+
             }
 
-        })
+        });
+        document.getElementById("search").reset();
     });
 
 });
 
 function appendDom(data) {
+    clear();
     console.log("I'm appending");
     for (var i = 0; i < data.length; i++) {
         $("#showAnimal").append("<div class = 'person'>" + "<p>"+ "Name: " + data[i].name + "</p>" +
             "<p>" + "Spirit Animal: " + data[i].spiritAnimal + "</p>" + "</div>");
         }
 };
+
+function clear(){
+    $("#showAnimal").empty();
+}
